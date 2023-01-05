a467bf8734fd288dd3dec549aea1f7fe33142488
Kim GyeongMin <gyeongmin951@naver.com>
2023-01-05 11:21:56 +0900

    :tada: [ Add ] first commit, app 추가, 초기 세팅
    users, books 앱 추가
    .env파일 추가 & secret key 설정
    test url설정

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import UserViewSet
+
+
+
+urlpatterns = [
+    path('', UserViewSet.as_view({'post':'create'})),
+]
---
+++ users/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+# Create your views here.
+
+
+class UserViewSet(ViewSet):
+    pass
