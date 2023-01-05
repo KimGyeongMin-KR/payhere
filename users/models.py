9a6a7ad322252e610810cfb073ec4662f782b69c
Kim GyeongMin <gyeongmin951@naver.com>
2023-01-05 11:42:24 +0900

    :heavy_plus_sign: [ Add ] User 모델 추가 & settings 설정

--- users/models.py
+++ users/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
+
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email or not username:
+            raise ValueError('Users must have an email address and username')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(
+            email=email,
+            password=password,
+            username=username,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username',]
+
+    def __str__(self):
+        return f'{self.username}({self.email})'
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.is_admin
+
+    class Meta:
+        db_table = 'users'
